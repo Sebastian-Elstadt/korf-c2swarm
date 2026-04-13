f42cfe4ba9441b68d2ac4a5a06c1e4a339e0b17a
Sebastian <sebastian@elstadt.com>
2026-04-13 04:50:47 +0200

    kommander/nodecom commands dispatching

--- kommander/nodecom/src/lib.rs
+++ kommander/nodecom/src/lib.rs
@@ -1,6 +1,16 @@
-use domain::{AppContext, node::NodeLogNetworkProtocol};
-use std::{net::SocketAddr, sync::Arc, time::Duration};
-use tokio::time::sleep;
+use domain::{
+    AppContext,
+    node::{NodeCommandType, NodeLogNetworkProtocol},
+};
+use std::{
+    net::{SocketAddr, SocketAddrV4},
+    sync::Arc,
+    time::Duration,
+};
+use tokio::{
+    net::{UdpSocket, unix::SocketAddr},
+    time::{sleep, timeout},
+};
 
 mod udp;
 
@@ -31,13 +41,29 @@ async fn run_cmd_dispatcher(app_ctx: Arc<AppContext>) {
         if let Err(err) = queue {
             eprintln!("nodecom: failed to read command queue: {err}");
             sleep(Duration::from_secs(2)).await;
-            continue;    
+            continue;
         }
 
         let queue = queue.unwrap();
+        if queue.len() == 0 {
+            sleep(Duration::from_secs(2)).await;
+            continue;
+        }
+
+        let udp_sock = UdpSocket::bind("0.0.0.0:0").await;
+        if let Err(err) = udp_sock {
+            eprintln!("nodecom: failed to bind local udp port: {err}");
+            sleep(Duration::from_secs(2)).await;
+            continue;
+        }
+
+        let udp_sock = udp_sock.unwrap();
 
         for cmd in queue.into_iter() {
-            let last_net_log = app_ctx.node_log_repo.get_last_network_log_by_node_id(cmd.node_id, Some(NodeLogNetworkProtocol::Udp)).await;
+            let last_net_log = app_ctx
+                .node_log_repo
+                .get_last_network_log_by_node_id(cmd.node_id, Some(NodeLogNetworkProtocol::Udp))
+                .await;
             if let Err(err) = last_net_log {
                 eprintln!("nodecom: failed to query last network log for command: {err}");
                 continue;
@@ -45,12 +71,75 @@ async fn run_cmd_dispatcher(app_ctx: Arc<AppContext>) {
 
             let last_net_log = last_net_log.unwrap();
             if last_net_log == None {
-                println!("nodecom: no last network log to execute command. (node id: {})", cmd.node_id);
+                println!(
+                    "nodecom: no last network log to execute command. (node id: {})",
+                    cmd.node_id
+                );
                 continue;
             }
 
             let last_net_log = last_net_log.unwrap();
-            // todo: send to ipv4+port
+            if last_net_log.ipv4_addr == None || last_net_log.network_port == None {
+                println!(
+                    "nodecom: no last network log contains invalid address. (node id: {})",
+                    cmd.node_id
+                );
+                continue;
+            }
+
+            let target_addr: SocketAddr = SocketAddrV4(
+                last_net_log.ipv4_addr.unwrap(),
+                last_net_log.network_port.unwrap(),
+            );
+
+            cmd.status = domain::node::NodeCommandStatus::Executing;
+            app_ctx.node_cmd_repo.update(&cmd).await;
+
+            let skip_send = false;
+            let mut cmd_buf: Vec<u8> = vec![77, 33, cmd.command_type as u8];
+            match cmd.command_type {
+                NodeCommandType::ShellScript => {
+                    if let Some(text_content) = cmd.text_content
+                        && text_content.len() > 0
+                    {
+                        let text_content_bytes = text_content.as_bytes();
+                        let len = text_content_bytes.len() as u32;
+                        cmd_buf.extend_from_slice(len.to_be_bytes());
+                        cmd_buf.extend_from_slice(text_content_bytes);
+                    } else {
+                        skip_send = true;
+                    }
+                }
+                _ => {}
+            };
+
+            let node_responded = false;
+            if !skip_send {
+                udp_sock.send_to(&cmd_buf, target_addr);
+
+                let mut response_buf = [u8; 32];
+                let response_result = timeout(
+                    Duration::from_secs(10),
+                    udp_sock.recv_from(&mut response_buf),
+                )
+                .await;
+                // this isnt great, would be better to filter through any incoming packets until the end of the 10 seconds
+                // but, I need to get this POC done as soon as possible.
+                if let Ok(Ok((len, addr))) = response_result
+                    && addr == target_addr
+                    && response_buf[..len] == b"ACK"
+                {
+                    node_responded = true;
+                } else {
+                    cmd.status = domain::node::NodeCommandStatus::Queued;
+                    app_ctx.node_cmd_repo.update(&cmd).await;
+                }
+            }
+
+            if skip_send || node_responded {
+                cmd.status = domain::node::NodeCommandStatus::Completed;
+                app_ctx.node_cmd_repo.update(&cmd).await;
+            }
         }
 
         sleep(Duration::from_secs(2)).await;
